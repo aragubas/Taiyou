006db4fa49fb68db0b76d4dc111a16643ec5c9a1
Aragubas <47148297+aragubas@users.noreply.github.com>
2022-05-06 23:23:42 -0300

    funny

--- RakClient/src/components/Window/ContactList.vue
+++ RakClient/src/components/Window/ContactList.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
 import { getInstance } from "../../window-manager";
-import { defineProps, onMounted } from "@vue/runtime-core";
+import { defineProps, onMounted, ref, Ref } from "@vue/runtime-core";
+import { v4 } from "uuid";
 const props = defineProps<{ windowID: string }>();
+
+class Contact
+{
+  username: string;
+  status: string;
+  id: string;
+
+  constructor(username: string, status: string)
+  {
+    this.username = username;
+    this.status = status;
+    this.id = v4();
+  }
+}
+
+const contact_examples: Ref<Array<Contact>> = ref(new Array<Contact>( new Contact("sinasssssssssssssssssssssssssssssssssssss", "Enceirando"), new Contact("ceira", "Online") ));
 
 onMounted(() => {
   getInstance(props.windowID).title = "Contacts";
@@ -12,10 +29,73 @@ onMounted(() => {
 
 <template>
   <div class="wrapper">
+    <ol>
+      <li v-for="contact in contact_examples" :key="contact.id" class="contact">
+        <span class="profilepicture"></span>
 
+        <div class="username-box">
+          <p>{{contact.username}}</p>
+          <i>{{contact.status}}</i>
+        </div>
+      </li>
+    </ol>
   </div>
 </template>
 
 <style scoped>
+.wrapper
+{
+  padding: .5rem;
+}
 
+ol
+{
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+i
+{
+  font-size: .7rem;
+}
+
+.username-box
+{
+  display: flex;
+  align-items: center;
+  min-width: 50px;
+
+  flex: 1;
+  
+}
+
+.contact span.profilepicture
+{
+  width: 32px;
+  height: 32px;
+  min-width: 32px;
+  background: rgb(80, 82, 88);
+  border-radius: 100%;
+}
+
+.username-box p
+{
+  width: 100%;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+
+}
+
+
+.contact
+{
+  display: flex;
+  align-items: stretch;
+  gap: .5rem;
+  background: rgb(60, 62, 68);
+  border-radius: 4px;
+  padding: .4rem;
+}
 </style>
